bdb182861c14dea639d3be729008cc51e9c71888
Justin Akridge <Akridjus@gmail.com>
2023-08-21 22:18:56 -0400

    added input validation

--- M01/part2/main.cpp
+++ M01/part2/main.cpp
@@ -1,5 +1,190 @@
 #include <iostream>
+#include "StereoReceiver.h"
+
+std::string getManufacturer() const {
+  std::string input;
+  bool valid = false;
+  do {
+    std::cout << "Enter manufacturer: ";
+    std::cin >> input;
+    if (std::cin.good()) {
+      valid = true;
+    } else {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::cout << "Invalid input. Please enter an string: ";
+    }
+  } while (!valid);
+  return input;
+}
+
+
+std::string getModel() const {
+  std::string input;
+  bool valid = false;
+  do {
+    std::cout << "Enter the model of the car: ";
+    std::cin >> input;
+    if (std::cin.good()) {
+      valid = true;
+    } else {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::cout << "Invalid input. Please enter an the model of car: ";
+    }
+  } while (!valid);
+  return input;
+}
+
+std::string getSerial_number() const {
+  std::string input;
+  bool valid = false;
+  do {
+    std::cout << "Enter the serial_number: ";
+    std::cin >> input;
+    if (std::cin.good()) {
+      valid = true;
+    } else {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::cout << "Serial number is invalid. ";
+    }
+  } while (!valid);
+  return input;
+}
+
+int getWattage() const {
+  bool valid = false;
+  do {
+    int bass; 
+    std::cout << "Enter bass: ";
+    std::cin >> bass;
+    if (std::cin.good()) {
+      valid = true;
+    } else {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::cout << "Invalid input. Please enter an integer: ";
+    }
+  } while (!valid);
+}
+
+int getNumber_of_channels() const {
+  bool valid = false;
+  do {
+    int bass; 
+    std::cout << "Enter bass: ";
+    std::cin >> bass;
+    if (std::cin.good()) {
+      valid = true;
+    } else {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::cout << "Invalid input. Please enter an integer: ";
+    }
+  } while (!valid);
+}
+
+std::string getBand() const {
+  bool valid = false;
+  do {
+    int bass; 
+    std::cout << "Enter bass: ";
+    std::cin >> bass;
+    if (std::cin.good()) {
+      valid = true;
+    } else {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::cout << "Invalid input. Please enter an integer: ";
+    }
+  } while (!valid);
+}
+
+double getFrequency() const {
+  bool valid = false;
+  do {
+    int bass; 
+    std::cout << "Enter bass: ";
+    std::cin >> bass;
+    if (std::cin.good()) {
+      valid = true;
+    } else {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::cout << "Invalid input. Please enter an integer: ";
+    }
+  } while (!valid);
+}
+
+int getVolume() const {
+  bool valid = false;
+  do {
+    int bass; 
+    std::cout << "Enter bass: ";
+    std::cin >> bass;
+    if (std::cin.good()) {
+      valid = true;
+    } else {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::cout << "Invalid input. Please enter an integer: ";
+    }
+  } while (!valid);
+}
+
+bool getPower() const {
+  bool valid = false;
+  do {
+    int bass; 
+    std::cout << "Enter bass: ";
+    std::cin >> bass;
+    if (std::cin.good()) {
+      valid = true;
+    } else {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::cout << "Invalid input. Please enter an integer: ";
+    }
+  } while (!valid);
+}
+
+std::string getLights() const {
+  bool valid = false;
+  do {
+    int bass; 
+    std::cout << "Enter bass: ";
+    std::cin >> bass;
+    if (std::cin.good()) {
+      valid = true;
+    } else {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::cout << "Invalid input. Please enter an integer: ";
+    }
+  } while (!valid);
+}
+
+int getBass_booster() const {
+  //add in dial
+  bool valid = false;
+  do {
+    int bass; 
+    std::cout << "Enter bass: ";
+    std::cin >> bass;
+    if (std::cin.good()) {
+      valid = true;
+    } else {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::cout << "Invalid input. Please enter an integer: ";
+    }
+  } while (!valid);
+  
+}
 
 int main() {
-  
+  do {
+    std::cout << " 
+  } while (); 
 }
