1589aa3547de9fb683f377106bf50a95295145f4
Justin Akridge <Akridjus@gmail.com>
2023-08-29 14:18:44 -0400

    isdigit function

--- M02/lab1.cpp
+++ M02/lab1.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
 int main() {
-    
+    std::string input;
+    std::cout << "Enter a number: ";
+    std::cin >> input;
+    bool good_number = true;
+    for (int i = 0; i < input.size(); i++) {
+        if (!isdigit(input[i])) {
+            std::cout << "You entered an invalid character in postion: " << i+1 << '\n';
+            good_number = false;
+        }
+    }
+    if (good_number)
+        std::cout << "You entered a good number\n";
 }
