559e3529806ea9ce3f7a78adc29a05f89bbcdcf5
Justin Akridge <Akridjus@gmail.com>
2023-08-28 22:19:42 -0400

    changes

--- M02/date.cpp
+++ M02/date.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-#include <exception>
+#include <string>
 
 class Date {
 public:
+  Date();
   Date(int, int, int);
   void setMonth();
   void setDay();
@@ -10,111 +11,133 @@ public:
   std::string getMonth();
   int getDay();
   int getYear();
+  std::string date_to_string();
 private:
   int month;
   int day;
   int year;
 };
 
+Date::Date() {}
+
+std::string Date::date_to_string() {
+  std::string str_day = std::to_string(day);
+  std::string str_month = getMonth();
+  std::string str_year = std::to_string(year);
+  std::string date = str_month + " " + str_day + ", " + str_year;
+  return date;
+}
+
 void Date::setMonth() {
-  try {
-    std::cout << "Enter the month: ";
-    int initMonth;
-    std::cin >> initMonth;
-    if (initMonth < 1 || initMonth > 12) {
-      throw initMonth;
-    }
-  } catch(int month) {
-    std::cout << month << " is an invalid month.\n"; 
-  }
+  bool flag = false;
+  do {
+    try {
+      std::cout << "Enter the month: ";
+      int initMonth;
+      std::cin >> initMonth;
+      if (initMonth < 1 || initMonth > 12) {
+        throw initMonth;
+      } else {
+        month = initMonth;
+        flag = true;
+      }
+    } catch(int month) {
+      std::cout << month << " is an invalid month.\n"; 
+    } 
+  } while(!flag);
 }
 
 void Date::setDay() {
-  try {
-    std::cout << "Enter the day of the month: ";
-    int day;
-    std::cin >> day;
-    if (day < 0 || day > 31)
-      throw day;
-    switch(month) {
-      case 2 :
-        if (day > 28)
-          throw day;
-      case 4:
-        if (day > 30)
-          throw day;
-      case 6:
-        if (day > 30)
-          throw day;
-      case 9:
-        if (day > 30)
-          throw day;
-      case 11:
-        if (day > 30)
-          throw day;
-      default:
-        day = day;
+  bool flag = false;
+  do {
+    try {
+      std::cout << "Enter the day of the month: ";
+      int initDay;
+      std::cin >> initDay;
+      if (initDay < 0 || initDay > 31)
+        throw initDay;
+      switch(month) {
+        case 2:
+          if (initDay > 28)
+            throw initDay;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+          if (initDay > 30)
+            throw initDay;
+      }
+      day = initDay;
+      flag = true;
+    } catch(int initDay) {
+      std::cout << initDay << " is an invalid day of the month\n";
     }
-  } catch(day) {
-    std::cout << day << " is an invalid day of the month.\n";
-  }
+  } while(!flag);
 }
 
 void Date::setYear() {
-  try {
-    std::cout << "Enter the year: ";
-    int year;
-    std::cin >> year;
-    if (year < 1900 || year > 2020)
-      throw year;
-    year = year;
-  } catch(year) {
-    std::cout << year << " is an invalid year.\n"
+  bool flag = false;
+  do {
+    try {
+      std::cout << "Enter the year: ";
+      int initYear;
+      std::cin >> initYear;
+      if (initYear < 1900 || initYear > 2020)
+        throw initYear;
+      year = initYear;
+    } catch(int initYear) {
+      std::cout << initYear << " is an invalid year.\n";
+    }
+  } while(!flag);
 }
 
 std::string Date::getMonth() {
-  std::string months = ["January", "Feburary" , "March", "April", "May", "June",
-                        "July", "August", "September", "October", "November" , "December"];
-  switch(month) {
-    case 1:
-      return months[0]; 
-      break;
-    case 2:
-      return months[1]; 
-      break;
-    case 3:
-      return months[2]; 
-      break;
-    case 4:
-      return months[3]; 
-      break;
-    case 5:
-      return months[4]; 
-      break;
-    case 6:
-      return months[5]; 
-      break;
-    case 7:
-      return months[6]; 
-      break;
-    case 8:
-      return months[7]; 
-      break;
-    case 9:
-      return months[8]; 
-      break;
-    case 10:
-      return months[9]; 
-      break;
-    case 11:
-      return months[10]; 
-      break;
-    case 12:
-      return months[11]; 
-      break;
-    default:
-      std::cout << "You need to still enter a date.\n"
-      break;
+  std::string months[] = {"January", "Feburary" , "March", "April", "May", "June",
+                          "July", "August", "September", "October", "November" , "December"};
+  try {
+    switch(month) {
+      case 1:
+        return months[0]; 
+        break;
+      case 2:
+        return months[1]; 
+        break;
+      case 3:
+        return months[2]; 
+        break;
+      case 4:
+        return months[3]; 
+        break;
+      case 5:
+        return months[4]; 
+        break;
+      case 6:
+        return months[5]; 
+        break;
+      case 7:
+        return months[6]; 
+        break;
+      case 8:
+        return months[7]; 
+        break;
+      case 9:
+        return months[8]; 
+        break;
+      case 10:
+        return months[9]; 
+        break;
+      case 11:
+        return months[10]; 
+        break;
+      case 12:
+        return months[11]; 
+        break;
+      default:
+        throw month;
+        break;
+    }   
+  } catch (int month) {
+      std::cout << "You still need to enter a month";
   }
 }
 
@@ -135,24 +158,29 @@ Date::Date(int initMonth, int initDay, int initYear) {
 
 
 int main() {
-  try {
-    int month;
-    std::cout << "Enter the month: ";
-    std::cin >> month;
-    if (month < 1 || month > 12)
-      throw month
-    int day;
-    std::cout << "Enter the day: ";
-    std::cin >> day;
-    if (day < 1 || day  > 32)
-      throw day;
-    int year;
-    std::cout << "Enter the year: ";
-    std::cin >> year;
-    if (year < 1900 || year > 2020)
-      throw year;
-  } catch (int value) {
-    std::cout << value << " is out of range\n";
-  }
+  Date d1;
+  d1.setMonth();
+  d1.setDay();
+  d1.setYear();
+  std::string year = d1.date_to_string();
+  std::cout << year << std::endl;
+  //try {
+  //  int month;
+  //  std::cout << "Enter the month: ";
+  //  std::cin >> month;
+  //  if (month < 1 || month > 12)
+  //    throw month
+  //  int day;
+  //  std::cout << "Enter the day: ";
+  //  std::cin >> day;
+  //  if (day < 1 || day  > 32)
+  //    throw day;
+  //  int year;
+  //  std::cout << "Enter the year: ";
+  //  std::cin >> year;
+  //  if (year < 1900 || year > 2020)
+  //    throw year;
+  //} catch (int value) {
+  //  std::cout << value << " is out of range\n";
+  //}
 }
-
