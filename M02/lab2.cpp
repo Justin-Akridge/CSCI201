89c0b744786525c5773604311e92db35a50c5d98
Justin Akridge <Akridjus@gmail.com>
2023-08-31 16:35:49 -0400

    exception handling added: must compile with clang

--- M02/lab2.cpp
+++ M02/lab2.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
-#include <stdexcept>
-#include <cstddef>
+#include <cstdint>
 
-/*out of range
- * range error
- * overflow error
- * */
+//This program needs to be compiled with clang for std::byte 
+
 int main() {
-  bool valid_byte = false;
-  do {
-    try {
-      int8_t b;
-      std::cout << "Enter a number between -127 and 127: ";
-        throw b;
+  try {
+    while (true) {
+      std::cout << "Enter a value for a byte: ";
+      uint16_t input_value;
+      std::cin >> input_value; 
+      if (input_value < 0 || input_value > 255) {
+        throw std::out_of_range("Value is out of range of a byte");
       }
-    } catch (const std::out_of_range& e) {
-        std::cerr << "Out of range error: " << e.what() << '\n';
-    } catch (const std::range_error& e) {
-        std::cerr << "range error: " << e.what() << '\n';
-    } catch (const std::overflow_error& e) {
-        std::cerr << "Overflow error: " << e.what() << '\n';
+      std::byte byte_value = static_cast<std::byte>(input_value);
+      byte_value = byte_value + std::byte(1);
+      std::cout << "Byte value: " << static_cast<int>(byte_value) << std::endl;
     }
-  } while (!valid_byte);
+  } catch (const std::out_of_range &e) {
+      std::cerr << "Out of range error: " << e.what() << std::endl;
+  } catch (const std::range_error &e) {
+      std::cerr << "Range error: " << e.what() << std::endl;
+  } catch (const std::overflow_error &e) {
+      std::cerr << "Overflow error: " << e.what() << std::endl;
+  } catch (...) {
+      std::cerr << "An unexpected error occured." << std::endl;
+  }
+  return 0;
 }
