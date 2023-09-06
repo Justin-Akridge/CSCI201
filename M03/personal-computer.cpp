1ea981549957f2615ab9bbff84378189eeb3a45d
Justin Akridge <Akridjus@gmail.com>
2023-09-05 20:20:28 -0400

    fix: contructor added

--- M03/personal-computer.cpp
+++ M03/personal-computer.cpp
@@ -1,2 +1,36 @@
+#include "personal-computer.h"
 #include <iostream>
 
+personal_computer::personal_computer() {
+  manufacturer = "";
+  form_factor = "";
+  serial_number = "";
+  processor = "";
+  ram = 0;
+  string storage_type = "";
+  storage_size = 0;
+}
+
+personal_computer::personal_computer(std::string init_manufacturer,
+                                     std::string init_form_factor,
+                                     std::string init_serial_number,
+                                     std::string init_processor,
+                                     int init_ram, std::string init_storage_type,
+                                     int init_storage_size) {
+  manufacturer = init_manufacturer;
+  form_factor = init_form_factor;
+  serial_number = init_serial_number;
+  processor = init_processor;
+  ram = init_ram;
+  string storage_type = init_storage_type;
+  storage_size = init_storage_size;
+}
+
+
+std::string personal_computer::get_manufacturer() const;
+std::string personal_computer::get_form_factor() const;
+std::string personal_computer::get_serial_number() const;
+std::string personal_computer::get_processor() const;
+int personal_computer::get_ram() const;
+std::string personal_computer::get_storage_type() const;
+int personal_computer::get_storage_size() const;
--- M03/personal-computer.h
+++ M03/personal-computer.h
@@ -5,6 +5,24 @@
 class personal_computer {
 public:
   personal_computer();
+  personal_computer(std::string, std::string, std::string, std::string,
+                    int, std::string, int);
+  
+  std::string get_manufacturer() const;
+  std::string get_form_factor() const;
+  std::string get_serial_number() const;
+  std::string get_processor() const;
+  int get_ram() const;
+  std::string get_storage_type() const;
+  int get_storage_size() const;
+
+  void set_manufacturer();
+  void set_form_factor();
+  void set_serial_number();
+  void set_processor();
+  void set_ram();
+  void set_storage_type();
+  void set_storage_size();
 
 private:
   std::string manufacturer;
