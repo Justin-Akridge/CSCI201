85015897e8d1c97f385e6a22c4b17d53b7bfdbe2
Justin Akridge <Akridjus@gmail.com>
2023-09-08 15:13:47 -0400

    changes

--- M03/part2/text_input.cpp
+++ M03/part2/text_input.cpp
@@ -2,9 +2,11 @@
 #include <fstream>
 #include <string>
 #include <unordered_set>
+#include <algorithm>
 
 int main() {
   std::unordered_set<std::string> words;
+  std::unordered_set<std::string>::iterator itr;
   std::ifstream myfile ("input.txt");
   if (myfile.is_open()) {
     while (myfile.good()) {
@@ -18,4 +20,7 @@ int main() {
     }
   }
   myfile.close();
+  for (itr = words.begin(); itr != words.end(); itr++) {
+    std::cout << *itr << '\n';
+  } 
 }
