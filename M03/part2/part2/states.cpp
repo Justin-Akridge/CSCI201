6c7eb72cda7a6889e89db1daf77019118178ef3d
Justin Akridge <Akridjus@gmail.com>
2023-09-09 11:40:24 -0400

    fix: finished

--- M03/part2/part2/states.cpp
+++ M03/part2/part2/states.cpp
@@ -11,6 +11,14 @@ struct state {
   std::string flower;
 };
 
+void parse_string(std::string &s) {
+  for (int i = 0; i < s.size(); i++) {
+    if (s[i] == '-') {
+      s[i] = ' ';
+    }
+  }
+}
+
 int main() {
   std::vector<std::string> state_abbreviation = {"AL", "AK", "AZ", "AR", "CA", 
                               "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", 
@@ -25,31 +33,49 @@ int main() {
   if (file.is_open()) {
     std::string line;
     int index = 0;
-    while(file.good() && !file.eof()) {
+    bool done = false;
+    while(!done) {
       state new_state;
       file >> new_state.state >> new_state.governor >> new_state.flower;
-      std::cout << new_state.state << new_state.governor << new_state.flower << std::endl;
+      parse_string(new_state.state);
+      parse_string(new_state.governor);
+      parse_string(new_state.flower);
       states[state_abbreviation[index]] = new_state;
       index++;
+      if (index == 50)
+        done = true;
     }
   }
   file.close();
   
   bool done = false;
   while (!done) {
-    std::cout << "Enter a two letter abbreviation of a state: ";
-    std::string state_selection;
-    std::cin >> state_selection;
-    std::cout << '\n';
-    for (int i = 0; i < state_selection.size(); i++) {
-      state_selection[i] = std::toupper(state_selection[i]);
-    }
-    for (itr = states.begin(); itr != states.end(); itr++) {
-      if (state_selection == itr->first) {
-        std::cout << "State: " << itr->second.state << '\n';
-        std::cout << "Governor: " << itr->second.governor << '\n';
-        std::cout << "Flower: " << itr->second.flower << "\n\n";
+    try {
+      std::cout << "Enter a two letter abbreviation of a state or -1 to quit: ";
+      std::string state_selection;
+      std::cin >> state_selection;
+      std::cout << '\n';
+      if (state_selection == "-1") {
+        done = true;
+        continue;
       }
+      for (int i = 0; i < state_selection.size(); i++) {
+        state_selection[i] = std::toupper(state_selection[i]);
+      }
+      bool is_valid_state = false;
+      for (itr = states.begin(); itr != states.end(); itr++) {
+        if (state_selection == itr->first) {
+          is_valid_state = true;
+          std::cout << "State: " << itr->second.state << '\n';
+          std::cout << "Governor: " << itr->second.governor << '\n';
+          std::cout << "Flower: " << itr->second.flower << "\n\n";
+        }      
+      }
+      if(!is_valid_state) {
+        throw std::invalid_argument("{ " + state_selection + " } abbreviation is not a valid state.\n\n");
+      }
+    } catch (std::invalid_argument &e) {
+      std::cerr << "Invalid argument: " << e.what();
     }
   }
 }
