0e47e68f5fc1c6de28bbf49727a71229478e5c20
Justin Akridge <Akridjus@gmail.com>
2023-09-09 14:54:13 -0400

    comments

---
+++ M03/part2/part1/text_input.cpp
@@ -0,0 +1,43 @@
+/*
+ * Justin Akridge
+ * 09/09/2023
+ * CSCI201
+ * This problem collects a stream of data from file and stores each word into a set without duplicate words.
+ */ 
+#include <iostream>
+#include <cctype>
+#include <fstream>
+#include <string>
+#include <set>
+#include <algorithm>
+
+void print(const std::string &word) {
+  std::cout << word << '\n';  
+}
+
+int main() {
+  std::set<std::string> words;
+  std::set<std::string>::iterator itr;
+  std::ifstream myfile ("input.txt");
+  if (myfile.is_open()) {
+    while (myfile.good()) {
+      std::string word = "";
+      myfile >> word;
+      if (word[0] == '.') {
+        word = word.substr(1, word.size()-1);
+      } else if (word[word.size()-1] == '.') {
+        word = word.substr(0, word.size()-1);
+      } 
+      for (auto& i : word) {
+        i = std::tolower(i);
+      }
+      if (words.count(word) >= 1) {
+        std::cerr << "error: " << "{ " << word << " }" << " is already in the set.\n";
+      } else {
+        words.insert(word); 
+      }
+    }
+  }
+  myfile.close();
+  std::for_each(words.begin(), words.end(), &print);
+}
