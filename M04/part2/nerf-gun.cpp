1dd63d8e057dd701c1d1fdd95b0210de6cb5420c
Justin Akridge <Akridjus@gmail.com>
2023-09-15 15:35:29 -0400

    class implemented

--- M04/part2/nerf-gun.cpp
+++ M04/part2/nerf-gun.cpp
@@ -1,22 +1,79 @@
-#ifndef NERF_GUN_H
-#define NERF_GUN_H
-#include <string>
+#include <iostream>
+#include "nerf-gun.h"
 
-class Nerf_gun {
-public:
-  Nerf_gun();
-  Nerf_gun(std::string, int, int);
-  std::string get_model() const;
-  int get_range() const;
-  int get_capacity() const;
-  int get_darts() const;
-  void set_model(std::string);
-  
-private:
-  std::string model;
-  int range;
-  int capacity;
-  int darts;
-};
+Nerf_gun() {
+  model = "";
+  range = 0;
+  capacity = 0;
+  darts = 0;
+}
 
-#endif
+Nerf_gun(std::string init_model, int init_range, int init_capacity) {
+  model = init_model;
+  range = init_range;
+  capacity = init_capacity;
+  darts = 30;
+}
+
+std::string get_model() const {
+  return model;
+}
+
+int get_range() const {
+  return range;
+}
+
+int get_capacity() const {
+  return capacity;
+}
+
+int get_darts() const {
+  return darts;
+}
+
+void set_model(std::string new_model) {
+  model = new_model;
+}
+
+void set_range(int new_range) {
+  range = new_range;
+}
+
+void set_capacity(int new_capacity) {
+  capacity = new_capacity;
+}
+
+void set_darts(int new_dart_amount) {
+  darts = new_dart_amount;
+} 
+
+bool operator==(const Nerf_gun &gun) const {
+  return (darts == gun.darts);
+}
+
+bool operator<(const Nerf_gun &gun) const {
+  return (darts < gun.darts);
+}
+
+Nerf_gun& operator--() const {
+  try {
+    if (darts <= 0) {
+      throw std::out_of_range("You are out of ammo. You need to reload.");
+    }
+    --darts;
+  } catch (std::out_of_range &e) {
+    std::cerr << e.what() << '\n';
+  }
+}
+
+Nerf_gun& operator+=() const;
+std::osstream operator<<(const Nerf_gun &gun) const;
+int main() {
+}
+
+/*
+ * std::string model;
+ * int range;
+ * int capacity;
+ * int darts;
+ */
---
+++ M04/part2/nerf-gun.h
@@ -0,0 +1,29 @@
+#ifndef NERF_GUN_H
+#define NERF_GUN_H
+#include <string>
+
+class Nerf_gun {
+public:
+  Nerf_gun();
+  Nerf_gun(std::string, int, int);
+  std::string get_model() const;
+  int get_range() const;
+  int get_capacity() const;
+  int get_darts() const;
+  void set_model(std::string);
+  void set_range(int);
+  void set_capacity(int);
+  void set_darts(int); 
+  bool operator==(const Nerf_gun &gun) const;
+  bool operator<(const Nerf_gun &gun) const;
+  Nerf_gun& operator--(const Nerf_gun &gun) const;
+  Nerf_gun& operator+=(const Nerf_gun &gun) const;
+  std::osstream operator<<(const Nerf_gun &gun) const;
+private:
+  std::string model;
+  int range;
+  int capacity;
+  int darts;
+};
+
+#endif
