15fba2662ed088d69e6073da2ae50c599666e8bd
Justin Akridge <Akridjus@gmail.com>
2023-09-24 22:55:19 -0400

    (feat:add) initial commit

---
+++ bank-management-system/Bank.h
@@ -0,0 +1,14 @@
+#ifndef BANK_H
+#define BANK_H
+
+class Bank {
+public:
+
+  
+private:
+  double balance;
+  double credit_line;
+  unordered_map<int, int> accounts;
+};
+
+#endif
---
+++ bank-management-system/Customer.h
@@ -0,0 +1,13 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+class Customer {
+public:
+  void withdraw();
+  void deposit(double);
+  double get_loan();
+ :
+private:
+};
+
+#endif
